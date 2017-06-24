5c433738a9feea0684107d5b898ff83ddeb5e226
thanassisKalv <thanassiskalv@gmail.com>
2017-06-24 16:38:19 +0300

    Update _cuda_shim.cu

--- include/_cuda_shim.cu
+++ include/_cuda_shim.cu
@@ -9,12 +9,20 @@ gpu_maxout(float* best, int* which,
     maxout<<<B/16,16>>>(best, which, cands, B, O, P);
 }
 
-
+// 32 CUDA blocks is reasonable number for most modern GPU's
+// Nvidia guidelines suggest a number relative to GPU's SMs
 void
 gpu_mean_pool(float* means,
     const float* X, const int* lengths, int B, int T, int O)
 {
-  mean_pool<<<B, 1>>>(means, X, lengths, B, T, O);
+  int numofBlocks;
+  if(B<32)
+	numofBlocks = B;
+  else
+	numofBlocks = 32;
+
+  mean_pool<<<numofBlocks, O>>>(means, X, lengths, B, T, O);
+    
 }
 
 
@@ -22,14 +30,28 @@ void
 gpu_max_pool(float* maxes, int* which,
     const float* X, const int* lengths, int B, int T, int O)
 {
-  max_pool<<<B, 1>>>(maxes, which, X, lengths, B, T, O);
+  int numofBlocks;
+  if(B<32)
+	numofBlocks = B;
+  else
+	numofBlocks = 32;
+
+  max_pool<<<numofBlocks, O>>>(maxes, which, X, lengths, B, T, O);
+
 }
 
 
 void
 gpu_backprop_mean_pool(float* dX, const float* d_means, const int* lengths, int B, int T, int O)
 {
-  backprop_mean_pool<<<B, 1>>>(dX, d_means, lengths, B, T, O);
+  int numofBlocks;
+  if(B<32)
+	numofBlocks = B;
+  else
+	numofBlocks = 32;
+
+  backprop_mean_pool<<<numofBlocks, O>>>(dX, d_means, lengths, B, T, O);
+
 }
 
 
@@ -37,7 +59,15 @@ void
 gpu_backprop_max_pool(float* dX, const float* d_maxes, const int* which,
 		      const int* lengths, int B, int T, int O)
 {
-  backprop_max_pool<<<B, 1>>>(dX, d_maxes, which, lengths, B, T, O);
+  int numofBlocks;
+  if(B<32)
+	numofBlocks = B;
+  else
+	numofBlocks = 32;
+	
+  backprop_max_pool<<<numofBlocks, O>>>(dX, d_maxes, which, lengths, B, T, O);
+
+
 }
 
 void
@@ -46,4 +76,3 @@ gpu_hash_data(char* dest,
 {
     hash_data<<<n_items,1>>>(dest, src, out_size, in_size, n_items, seed);
 }
-
